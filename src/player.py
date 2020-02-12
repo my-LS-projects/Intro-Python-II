81f0f593e23033c92964fbf17b7577aa2e2576b4
Dakotah Huey <hueydakotah@gmail.com>
2020-02-12 18:25:59 -0500

    initial commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, items, room):
+        self.name = name
+        self.items = items
+        self.room = room
