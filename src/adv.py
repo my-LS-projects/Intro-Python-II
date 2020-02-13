d7d9c01757ff37571903da1bde276db30aa55204
Dakotah Huey <hueydakotah@gmail.com>
2020-02-13 02:20:03 -0500

    refactoring adv

--- src/adv.py
+++ src/adv.py
@@ -1,48 +1,11 @@
 from room import Room
+from roomlist import room
+from player import Player
+from item import Item
+import sys
 
 # Declare all the rooms
 
-room = {
-    "outside": Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
-    "foyer": Room(
-        "Foyer",
-        """Dim light filters in from the south. Dusty
-passages run north and east.""",
-    ),
-    "overlook": Room(
-        "Grand Overlook",
-        """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""",
-    ),
-    "narrow": Room(
-        "Narrow Passage",
-        """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""",
-    ),
-    "treasure": Room(
-        "Treasure Chamber",
-        """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""",
-    ),
-}
-
-
-# Link rooms together
-
-room["outside"].n_to = room["foyer"]
-room["foyer"].s_to = room["outside"]
-room["foyer"].n_to = room["overlook"]
-room["foyer"].e_to = room["narrow"]
-room["overlook"].s_to = room["foyer"]
-room["narrow"].w_to = room["foyer"]
-room["narrow"].n_to = room["treasure"]
-room["treasure"].s_to = room["narrow"]
-
-#
-# Main
-#
 
 # Make a new player object that is currently in the 'outside' room.
 
@@ -57,11 +20,30 @@ room["treasure"].s_to = room["narrow"]
 #
 # If the user enters "q", quit the game.
 
-while True:
-    print(room["outside"])
-    p_input = input("---> ")
-    if p_input == "w":
-        print("hello")
-    elif p_input == "q":
-        print("See you next time...")
-        break
+commands = ["n", "s", "e", "w", "take", "drop", "q", "quit"]
+
+#
+# Main
+#
+
+
+def main():
+    # create player to initialize game
+    player = Player(input("Player name: "), room["outside"])
+    print(f"Welcome, {player.name}")
+
+    while True:
+        print("Current location: ", player.current_room.name)
+        print(player.current_room.description)
+        command = input("What's the move? [n/s/e/w/take/drop]  ").lower()
+
+        # check if valid command
+        if command in commands:
+            if command == "q" or command == "quit":
+                print("See you next time.")
+                sys.exit()
+        else:
+            print("Sorry, that's not a valid command. Try: [n/s/e/w/q/quit]")
+
+
+main()
