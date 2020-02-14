d9d5674ab1994262c7dbdb59ab7e8fa7dd456ea7
Dakotah Huey <hueydakotah@gmail.com>
2020-02-13 21:32:44 -0500

    feat: player methods for take and drop WIP

--- src/player.py
+++ src/player.py
@@ -3,11 +3,22 @@
 
 
 class Player:
-    def __init__(self, name, current_room, items=[]):
+    def __init__(self, name, current_room, inventory=[]):
         self.name = name
         self.current_room = current_room
-        self.items = items
+        self.inventory = inventory
 
     def __str__(self):
         return f"{self.name}, {self.current_room}"
 
+    def move(self, command):
+        next_room = getattr(self.current_room, f"{command}_to")
+        if next_room is not None:
+            self.current_room = next_room
+            print(f"You walk towards the {next_room.name}...")
+
+    def yoink(self, item):
+        item = getattr(self.current_room.items, f"{item}")
+
+    def drop(self, item):
+        item = getattr(self.inventory, f"{item}")
